fbb7ce7b8a183c74445c4fa28c1d378c4154a7d8
Emerson Leite <83618300+e-leite@users.noreply.github.com>
2023-07-21 19:57:45 -0300

    feat: create material.module

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const MODULES = [MatToolbarModule];
+
+@NgModule({
+  declarations: [],
+  imports: [MODULES],
+  exports: [MODULES],
+})
+export class MaterialModule {}
