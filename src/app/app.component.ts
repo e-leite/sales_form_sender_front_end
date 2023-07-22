16083e6071cd820576b7822431a364202105c863
Emerson Leite <83618300+e-leite@users.noreply.github.com>
2023-07-22 08:59:58 -0300

    refactor: add toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'sales_form_sender_front_end';
+  title = 'Sales Form Sender';
 }
