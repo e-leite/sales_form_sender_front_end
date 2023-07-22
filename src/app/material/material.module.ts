d4fb2c5350cbe6995588e3af0d886a19bc59c801
Emerson Leite <83618300+e-leite@users.noreply.github.com>
2023-07-22 12:20:01 -0300

    feat: add imports in material.modules

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
-const MODULES = [MatToolbarModule, MatIconModule, MatButtonModule];
+const MODULES = [
+  MatButtonModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatPaginatorModule,
+  MatSelectModule,
+  MatTableModule,
+  MatToolbarModule,
+];
 
 @NgModule({
   declarations: [],
