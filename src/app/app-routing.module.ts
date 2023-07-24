d0c5e7d4528abe754d6209d27df8f9ab31c43e0f
Emerson Leite <83618300+e-leite@users.noreply.github.com>
2023-07-24 20:26:40 -0300

    feat: add route to SalesFormComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SalesOrderListComponent } from './sales-order-list/sales-order-list.component';
+import { SalesOrderFormComponent } from './sales-order-form/sales-order-form.component';
 
 const routes: Routes = [];
 
@@ -9,6 +10,7 @@ const routes: Routes = [];
   imports: [
     RouterModule.forRoot([
       { path: 'sales', component: SalesOrderListComponent },
+      { path: 'sales/new', component: SalesOrderFormComponent },
     ]),
   ],
   exports: [RouterModule],
