d247f4875d618dac932b3b8befaceecc43f5e34e
Emerson Leite <83618300+e-leite@users.noreply.github.com>
2023-07-24 20:30:47 -0300

    feat: add FormsModule ReactiveFormsModule and SalesOrderFormComponent to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { SalesOrderListComponent } from './sales-order-list/sales-order-list.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SalesOrderFormComponent } from './sales-order-form/sales-order-form.component';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 @NgModule({
-  declarations: [AppComponent, SalesOrderListComponent],
+  declarations: [
+    AppComponent,
+    SalesOrderListComponent,
+    SalesOrderFormComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
     MaterialModule,
   ],
-  providers: [],
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'en-GB' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
