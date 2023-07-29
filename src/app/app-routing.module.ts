9d4a1ca3650ecdb188b2fe7bf12c24526dc1754d
Emerson Leite <83618300+e-leite@users.noreply.github.com>
2023-07-29 08:10:46 -0300

    feat: add SalesOrderItemListComponent in routing-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { SalesOrderListComponent } from './sales-order-list/sales-order-list.component';
 import { SalesOrderFormComponent } from './sales-order-form/sales-order-form.component';
 import { ProductsFormComponent } from './products-form/products-form.component';
+import { SalesOrderItemListComponent } from './sales-order-item-list/sales-order-item-list.component';
 
 const routes: Routes = [];
 
@@ -13,6 +14,10 @@ const routes: Routes = [];
       { path: 'sales', component: SalesOrderListComponent },
       { path: 'sales/new', component: SalesOrderFormComponent },
       { path: 'sales/:id/products/add', component: ProductsFormComponent },
+      {
+        path: 'sales/:id/products/list',
+        component: SalesOrderItemListComponent,
+      },
     ]),
   ],
   exports: [RouterModule],
