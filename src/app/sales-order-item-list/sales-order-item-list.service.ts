501b8f8f1a692328c373613b1d6545d2ba6272c8
Emerson Leite <83618300+e-leite@users.noreply.github.com>
2023-07-31 20:13:24 -0300

    feat: create save and delete methods in SalesOrderItemListService

--- src/app/sales-order-item-list/sales-order-item-list.service.ts
+++ src/app/sales-order-item-list/sales-order-item-list.service.ts
@@ -9,11 +9,29 @@ import { SALES_ITEM_DATA } from 'src/assets/SalesItens-mock';
 export class SalesOrderItemListService {
   constructor() {}
 
+  save(salesItem: SalesItem): void {
+    const id = SALES_ITEM_DATA.length + 1;
+
+    salesItem.id = id;
+
+    SALES_ITEM_DATA.push(salesItem);
+  }
+
   get(id: number): Observable<SalesItem[]> {
     const dbResult = SALES_ITEM_DATA.filter(
       (itens: SalesItem) => itens.salesOrderId == id
     );
 
     return of(dbResult);
+  }
+
+  delete(salesItem: SalesItem): Observable<void> {
+    const findedItemIndex = SALES_ITEM_DATA.findIndex(
+      (item) => item.id == salesItem.id
+    );
+
+    SALES_ITEM_DATA.splice(findedItemIndex, 1);
+
+    return of(void 0);
   }
 }
